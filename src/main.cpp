94ce9e8f917852a0d7423bf39458a3b48e330456
Maxime26s <35577093+Maxime26s@users.noreply.github.com>
2024-12-14 22:14:16 -0500

    1.3 - Opcode Patterns in 8086 Arithmetic

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,50 @@
       ((byte) & 0x08 ? '1' : '0'), ((byte) & 0x04 ? '1' : '0'),                \
       ((byte) & 0x02 ? '1' : '0'), ((byte) & 0x01 ? '1' : '0')
 
+#define INSTRUCTION_NOT_IMPLEMENTED()                                          \
+  printf("\n" BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(instruction[0]));         \
+  printf(" - INSTRUCTION NOT IMPLEMENTED\n");                                  \
+  exit(-1);
+
+#define OPCODE_MOV_REGMEM2REG 0b10001000
+#define OPCODE_MOV_IMM2REGMEM 0b11000110
+#define OPCODE_MOV_IMM2REG 0b10110000
+#define OPCODE_MOV_MEM2ACC 0b10100000
+#define OPCODE_MOV_ACC2MEM 0b10100010
+
+#define OPCODE_ADD_REGMEM2REG 0b00000000
+#define OPCODE_ADD_IMM2REGMEM 0b10000000
+#define OPCODE_ADD_IMM2ACC 0b00000100
+
+#define OPCODE_SUB_REGMEM2REG 0b00101000
+#define OPCODE_SUB_IMM2REGMEM 0b10000000
+#define OPCODE_SUB_IMM2ACC 0b00101100
+
+#define OPCODE_CMP_REGMEM2REG 0b00111000
+#define OPCODE_CMP_IMM2REGMEM 0b10000000
+#define OPCODE_CMP_IMM2ACC 0b00111100
+
+#define OPCODE_JE 0b01110100
+#define OPCODE_JL 0b01111100
+#define OPCODE_JLE 0b01111110
+#define OPCODE_JB 0b01110010
+#define OPCODE_JBE 0b01110110
+#define OPCODE_JP 0b01111010
+#define OPCODE_JO 0b01110000
+#define OPCODE_JS 0b01111000
+#define OPCODE_JNE 0b01110101
+#define OPCODE_JNL 0b01111101
+#define OPCODE_JG 0b01111111
+#define OPCODE_JNB 0b01110011
+#define OPCODE_JA 0b01110111
+#define OPCODE_JNP 0b01111011
+#define OPCODE_JNO 0b01110001
+#define OPCODE_JNS 0b01111001
+#define OPCODE_LOOP 0b11100010
+#define OPCODE_LOOPZ 0b11100001
+#define OPCODE_LOOPNZ 0b11100000
+#define OPCODE_JCXZ 0b11100011
+
 // name is a 3 byte buffer;
 static uint32_t GetReg(uint8_t data, uint8_t is_word, char *buffer) {
   switch (data) {
@@ -50,7 +94,7 @@ static uint32_t GetReg(uint8_t data, uint8_t is_word, char *buffer) {
 
 // name is a 6 byte buffer;
 static uint32_t GetEffectiveAddress(uint8_t data, char *buffer) {
-  if (((data & 0b11000111) ^ 0b00000110) == 0) {
+  if ((data & 0b11000111) == 0b00000110) {
     return 0;
   }
   switch (data & 0b00000111) {
@@ -90,10 +134,14 @@ static uint32_t GetEffectiveAddress(uint8_t data, char *buffer) {
   return 0;
 }
 
-uint32_t GetValue(uint8_t *data, bool is_wide, uint16_t *value,
+uint32_t GetValue(uint8_t *data, bool is_signed, bool is_wide, uint16_t *value,
                   char *data_buf) {
   *value = data[0];
-  if (is_wide) {
+  if (is_signed) {
+    *value |= (*value & 0b10000000) << 8;
+    strncpy(data_buf, "%hd", 4);
+    return 1;
+  } else if (is_wide) {
     *value += (data[1] << 8);
     strncpy(data_buf, "%hd", 4);
     return 2;
@@ -104,7 +152,7 @@ uint32_t GetValue(uint8_t *data, bool is_wide, uint16_t *value,
 }
 
 uint32_t PrintEffectiveAddress(uint8_t *instruction, char *data_buf) {
-  uint32_t additional_bytes = 0;
+  uint32_t optional_byte_count = 0;
   uint8_t mod = instruction[1] & 0b11000000;
 
   uint32_t count = GetEffectiveAddress(instruction[1], data_buf);
@@ -120,13 +168,121 @@ uint32_t PrintEffectiveAddress(uint8_t *instruction, char *data_buf) {
     }
 
     uint16_t value;
-    additional_bytes = GetValue(&instruction[2], mod > 0b01000000 || count == 0,
-                                &value, data_buf);
+    optional_byte_count =
+        GetValue(&instruction[2], false, mod > 0b01000000 || count == 0, &value,
+                 data_buf);
     printf(data_buf, value);
   }
   printf("]");
 
-  return additional_bytes;
+  return optional_byte_count;
+}
+
+uint32_t RegisterOrMemoryWithRegisterToEither(uint8_t *instruction,
+                                              char *buffer) {
+  uint8_t is_dest = instruction[0] & 0b00000010;
+  uint8_t is_wide = instruction[0] & 0b00000001;
+  uint8_t mod = instruction[1] & 0b11000000;
+
+  uint32_t optional_byte_count = 0;
+  uint32_t buffer_size = 0;
+
+  if (is_dest) {
+    buffer_size = GetReg((instruction[1] & 0b00111000) >> 3, is_wide, buffer);
+    printf("%.*s", (int)buffer_size, buffer);
+    printf(", ");
+  }
+
+  if (mod == 0b11000000) {
+    buffer_size = GetReg((instruction[1] & 0b00000111), is_wide, buffer);
+    printf("%.*s", (int)buffer_size, buffer);
+  } else {
+    optional_byte_count += PrintEffectiveAddress(instruction, buffer);
+  }
+
+  if (!is_dest) {
+    printf(", ");
+    buffer_size = GetReg((instruction[1] & 0b00111000) >> 3, is_wide, buffer);
+    printf("%.*s", (int)buffer_size, buffer);
+  }
+
+  return optional_byte_count;
+}
+
+uint32_t ImmediateToRegisterOrMemory(uint8_t *instruction, char *buffer,
+                                     bool signed_op) {
+  uint8_t is_signed = signed_op ? instruction[0] & 0b00000010 : 0;
+  uint8_t is_wide = instruction[0] & 0b00000001;
+  uint8_t mod = instruction[1] & 0b11000000;
+
+  uint32_t optional_byte_count = 0;
+  uint32_t buffer_size = 0;
+
+  if ((mod ^ 0b11000000) == 0) {
+    buffer_size = GetReg((instruction[1] & 0b00000111), is_wide, buffer);
+    printf("%.*s", (int)buffer_size, buffer);
+  } else {
+    printf(is_wide ? "word " : "byte ");
+    optional_byte_count += PrintEffectiveAddress(instruction, buffer);
+  }
+  printf(", ");
+
+  uint16_t value;
+  optional_byte_count += GetValue(&instruction[2 + optional_byte_count],
+                                  is_signed, is_wide, &value, buffer) -
+                         1;
+  printf(buffer, value);
+
+  return optional_byte_count;
+}
+
+uint32_t ImmediateToRegister(uint8_t *instruction, char *buffer,
+                             uint8_t is_wide, uint8_t reg) {
+
+  uint32_t optional_byte_count = 0;
+  uint32_t buffer_size = 0;
+
+  buffer_size = GetReg(reg, is_wide, buffer);
+  printf("%.*s, ", (int)buffer_size, buffer);
+
+  uint16_t value;
+  optional_byte_count +=
+      GetValue(&instruction[1], false, is_wide, &value, buffer) - 1;
+  printf(buffer, value);
+
+  return optional_byte_count;
+}
+
+uint32_t ImmediateToAccumulator(uint8_t *instruction, char *buffer) {
+  uint8_t is_wide = instruction[0] & 0b00000001;
+  return ImmediateToRegister(instruction, buffer, is_wide, 0);
+}
+
+uint32_t AddressWithAccumulatorToEither(uint8_t *instruction, char *buffer) {
+  uint8_t is_dest = !(instruction[0] & 0b00000010);
+  uint8_t is_wide = instruction[0] & 0b00000001;
+
+  uint32_t optional_byte_count = 0;
+  uint32_t buffer_size = 0;
+
+  if (is_dest) {
+    buffer_size = GetReg(0, is_wide, buffer);
+    printf("%.*s, ", (int)buffer_size, buffer);
+  }
+
+  printf("[");
+  uint16_t value;
+  optional_byte_count +=
+      GetValue(&instruction[1], false, is_wide, &value, buffer) - 1;
+  printf(buffer, value);
+  printf("]");
+
+  if (!is_dest) {
+    buffer_size = GetReg(0, is_wide, buffer);
+    printf(", %.*s", (int)buffer_size, buffer);
+  }
+
+  return optional_byte_count;
 }
 
 int main(int argc, char *argv[]) {
@@ -166,103 +322,176 @@ int main(int argc, char *argv[]) {
   printf("; %s\n", filename);
   printf("bits 16\n");
   for (long i = 0; i < fileSize;) {
-    uint32_t additional_bytes = 0;
+    uint32_t instruction_byte_count = 0;
     uint8_t *instruction = &buffer[i];
-    // printf("filesize: %ld, i: %ld, byte: %.*s\n", fileSize, i, 6, byte);
-    if (((instruction[0] & 0b11111100) ^ 0b10001000) == 0) {
-      // mov
+
+    if ((instruction[0] & 0b11111100) == OPCODE_MOV_REGMEM2REG) {
       printf("mov ");
-
-      uint8_t is_wide = instruction[0] & 0b00000001;
-      uint8_t mod = instruction[1] & 0b11000000;
-      uint8_t is_dest = instruction[0] & 0b00000010;
-      if (is_dest) {
-        // first reg is destination
-        count = GetReg((instruction[1] & 0b00111000) >> 3, is_wide, data_buf);
-        printf("%.*s", (int)count, data_buf);
-        printf(", ");
-      }
-
-      if ((mod ^ 0b11000000) == 0) {
-        // register mode
-        count = GetReg((instruction[1] & 0b00000111), is_wide, data_buf);
-        printf("%.*s", (int)count, data_buf);
-      } else {
-        i += PrintEffectiveAddress(instruction, data_buf);
-      }
-
-      if (!is_dest) {
-        // first reg is source
-        printf(", ");
-        count = GetReg((instruction[1] & 0b00111000) >> 3, is_wide, data_buf);
-        printf("%.*s", (int)count, data_buf);
-      }
-      i += 2;
-    } else if (((instruction[0] & 0b11110000) ^ 0b10110000) == 0) {
-      // mov
+      instruction_byte_count = 2;
+      instruction_byte_count +=
+          RegisterOrMemoryWithRegisterToEither(instruction, data_buf);
+    } else if ((instruction[0] & 0b11110000) == OPCODE_MOV_IMM2REG) {
       printf("mov ");
+      instruction_byte_count = 2;
       uint8_t is_wide = instruction[0] & 0b00001000;
-
-      count = GetReg((instruction[0] & 0b00000111), is_wide, data_buf);
-      printf("%.*s, ", (int)count, data_buf);
-
-      uint16_t value;
-      i += GetValue(&instruction[1], is_wide, &value, data_buf);
-      printf(data_buf, value);
-      ++i;
-    } else if (((instruction[0] & 0b11111110) ^ 0b11000110) == 0) {
-      // mov
+      uint8_t reg = instruction[0] & 0b00000111;
+      instruction_byte_count +=
+          ImmediateToRegister(instruction, data_buf, is_wide, reg);
+    } else if ((instruction[0] & 0b11111110) == OPCODE_MOV_IMM2REGMEM) {
       printf("mov ");
-
-      uint8_t is_wide = instruction[0] & 0b00000001;
-      uint8_t mod = instruction[1] & 0b11000000;
-
-      if ((mod ^ 0b11000000) == 0) {
-        // register mode
-        count = GetReg((instruction[1] & 0b00000111), is_wide, data_buf);
-        printf("%.*s", (int)count, data_buf);
+      instruction_byte_count = 3;
+      instruction_byte_count +=
+          ImmediateToRegisterOrMemory(instruction, data_buf, false);
+    } else if ((instruction[0] & 0b11111100) ==
+               ((OPCODE_MOV_MEM2ACC | OPCODE_MOV_ACC2MEM) & 0b11111100)) {
+      printf("mov ");
+      instruction_byte_count = 2;
+      instruction_byte_count +=
+          AddressWithAccumulatorToEither(instruction, data_buf);
+    } else if ((instruction[0] & 0b11111100) == OPCODE_ADD_REGMEM2REG ||
+               (instruction[0] & 0b11111100) == OPCODE_SUB_REGMEM2REG ||
+               (instruction[0] & 0b11111100) == OPCODE_CMP_REGMEM2REG) {
+      switch (instruction[0] & 0b00111000) {
+      case 0b00000000: {
+        printf("add ");
+      } break;
+      case 0b00101000: {
+        printf("sub ");
+      } break;
+      case 0b00111000: {
+        printf("cmp ");
+      } break;
+      default:
+        INSTRUCTION_NOT_IMPLEMENTED();
+      }
+      instruction_byte_count = 2;
+      instruction_byte_count +=
+          RegisterOrMemoryWithRegisterToEither(instruction, data_buf);
+    } else if ((instruction[0] & 0b11111100) ==
+               (OPCODE_ADD_IMM2REGMEM | OPCODE_SUB_IMM2REGMEM |
+                OPCODE_CMP_IMM2REGMEM)) {
+      switch (instruction[1] & 0b00111000) {
+      case 0b00000000: {
+        printf("add ");
+      } break;
+      case 0b00101000: {
+        printf("sub ");
+      } break;
+      case 0b00111000: {
+        printf("cmp ");
+      } break;
+      default:
+        INSTRUCTION_NOT_IMPLEMENTED();
+      }
+      instruction_byte_count = 3;
+      instruction_byte_count +=
+          ImmediateToRegisterOrMemory(instruction, data_buf, true);
+    } else if ((instruction[0] & 0b11111110) == OPCODE_ADD_IMM2ACC ||
+               (instruction[0] & 0b11111110) == OPCODE_SUB_IMM2ACC ||
+               (instruction[0] & 0b11111110) == OPCODE_CMP_IMM2ACC) {
+      switch (instruction[0] & 0b00111000) {
+      case 0b00000000: {
+        printf("add ");
+      } break;
+      case 0b00101000: {
+        printf("sub ");
+      } break;
+      case 0b00111000: {
+        printf("cmp ");
+      } break;
+      default:
+        INSTRUCTION_NOT_IMPLEMENTED();
+      }
+      instruction_byte_count = 2;
+      instruction_byte_count += ImmediateToAccumulator(instruction, data_buf);
+    } else if (instruction[0] == OPCODE_JE || instruction[0] == OPCODE_JL ||
+               instruction[0] == OPCODE_JLE || instruction[0] == OPCODE_JB ||
+               instruction[0] == OPCODE_JBE || instruction[0] == OPCODE_JP ||
+               instruction[0] == OPCODE_JO || instruction[0] == OPCODE_JS ||
+               instruction[0] == OPCODE_JNE || instruction[0] == OPCODE_JNL ||
+               instruction[0] == OPCODE_JG || instruction[0] == OPCODE_JNB ||
+               instruction[0] == OPCODE_JA || instruction[0] == OPCODE_JNP ||
+               instruction[0] == OPCODE_JNO || instruction[0] == OPCODE_JNS ||
+               instruction[0] == OPCODE_LOOP ||
+               instruction[0] == OPCODE_LOOPZ ||
+               instruction[0] == OPCODE_LOOPNZ ||
+               instruction[0] == OPCODE_JCXZ) {
+      switch (instruction[0]) {
+      case OPCODE_JE: {
+        printf("je ");
+      } break;
+      case OPCODE_JL: {
+        printf("jl ");
+      } break;
+      case OPCODE_JLE: {
+        printf("jle ");
+      } break;
+      case OPCODE_JB: {
+        printf("jb ");
+      } break;
+      case OPCODE_JBE: {
+        printf("jbe ");
+      } break;
+      case OPCODE_JP: {
+        printf("jp ");
+      } break;
+      case OPCODE_JO: {
+        printf("jo ");
+      } break;
+      case OPCODE_JS: {
+        printf("js ");
+      } break;
+      case OPCODE_JNE: {
+        printf("jne ");
+      } break;
+      case OPCODE_JNL: {
+        printf("jnl ");
+      } break;
+      case OPCODE_JG: {
+        printf("jg ");
+      } break;
+      case OPCODE_JNB: {
+        printf("jnb ");
+      } break;
+      case OPCODE_JA: {
+        printf("ja ");
+      } break;
+      case OPCODE_JNP: {
+        printf("jnp ");
+      } break;
+      case OPCODE_JNO: {
+        printf("jno ");
+      } break;
+      case OPCODE_JNS: {
+        printf("jns ");
+      } break;
+      case OPCODE_LOOP: {
+        printf("loop ");
+      } break;
+      case OPCODE_LOOPZ: {
+        printf("loopz ");
+      } break;
+      case OPCODE_LOOPNZ: {
+        printf("loopnz ");
+      } break;
+      case OPCODE_JCXZ: {
+        printf("jcxz ");
+      } break;
+      default:
+        INSTRUCTION_NOT_IMPLEMENTED();
+      }
+      instruction_byte_count = 2;
+      int8_t value = instruction[1] + 2;
+      if (value < 0) {
+        printf("$+0%hhd", value);
       } else {
-        additional_bytes = PrintEffectiveAddress(instruction, data_buf);
+        printf("$+0+%hhd", value);
       }
-      printf(", ");
-
-      uint16_t value;
-      i += GetValue(&instruction[2 + additional_bytes], is_wide, &value,
-                    data_buf);
-      printf(is_wide ? "word " : "byte ");
-      printf(data_buf, value);
-
-      i += additional_bytes;
-      i += 2;
-    } else if (((instruction[0] & 0b11111100) ^ 0b10100000) == 0) {
-      // mov
-      printf("mov ");
-
-      uint8_t is_dest = !(instruction[0] & 0b00000010);
-      uint8_t is_wide = instruction[0] & 0b00000001;
-
-      if (is_dest) {
-        // first reg is destination
-        printf("ax, ");
-      }
-
-      printf("[");
-      uint16_t value;
-      i += GetValue(&instruction[1], is_wide, &value, data_buf);
-      printf(data_buf, value);
-      printf("]");
-
-      if (!is_dest) {
-        printf(", ax");
-      }
-      ++i;
     } else {
-    unhandled:
-      printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(instruction[0]));
-      printf("unsupported op code\n");
-      break;
+      INSTRUCTION_NOT_IMPLEMENTED();
     }
     printf("\n");
+    i += instruction_byte_count;
   }
 
   // Clean up
